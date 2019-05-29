d64e488d1f674f805faaa45b29086e8030baf660
kaito-h-personal <unrequal@gmail.com>
2019-05-29 16:47:44 +0900

    フォルダ構成の見直し修正

--- docker_files/create_local_env.sh
+++ docker_files/create_local_env.sh
@@ -4,6 +4,6 @@ docker-compose up
 
 # Laravelプロジェクトのmysqlの設定
 # 本当はcコマンドを使いたかったが、マニュアル通り動かなかったので妥協...
-sed -ie 's/DB_DATABASE=.*$/DB_DATABASE='${MYSQL_DATABASE}'/' ../${PJ_NAME}.env
-sed -ie 's/DB_USERNAME=.*$/DB_USERNAME='${MYSQL_USERNAME}'/' ../${PJ_NAME}.env
-sed -ie 's/DB_PASSWORD=.*$/DB_PASSWORD='${MYSQL_PASSWORD}'/' ../${PJ_NAME}.env
+sed -ie 's/DB_DATABASE=.*$/DB_DATABASE='${MYSQL_DATABASE}'/' ./${PJ_NAME}/.env
+sed -ie 's/DB_USERNAME=.*$/DB_USERNAME='${MYSQL_USERNAME}'/' ./${PJ_NAME}/.env
+sed -ie 's/DB_PASSWORD=.*$/DB_PASSWORD='${MYSQL_PASSWORD}'/' ./${PJ_NAME}/.env
