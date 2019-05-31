9aeaecad8eff643235ad5cf6de4d4cd3f634c972
kaito-h <kaito.hayafune@pasonadigisol.jp>
2019-05-31 17:38:56 +0900

    環境構築時にupでなくcreateする

--- docker_files/create_local_env.sh
+++ docker_files/create_local_env.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-# cacheを使うとDockerfileのcompose等が行われないことがある
+# appのDockerfileからimageを作成
+# cacheを使うとDockerfileのRUNが行われないことがある
 docker-compose build --no-cache
-docker-compose up
-# install Laravel
+
+# imageからコンテナを作成(起動はしない)
+docker-compose create
+
+# appコンテナ内からLaravelインストール
 docker-compose run app composer create-project --prefer-dist laravel/laravel ${PJ_NAME}
 
 # Laravelプロジェクトのmysqlの設定
-# 本当はcコマンドを使いたかったが、マニュアル通り動かなかったので妥協...
+# 本当はsedのcコマンドを使いたかったが、マニュアル通り動かなかったので妥協...
 sed -ie 's/DB_DATABASE=.*$/DB_DATABASE='${MYSQL_DATABASE}'/' ../${PJ_NAME}/.env
 sed -ie 's/DB_USERNAME=.*$/DB_USERNAME='${MYSQL_USERNAME}'/' ../${PJ_NAME}/.env
 sed -ie 's/DB_PASSWORD=.*$/DB_PASSWORD='${MYSQL_PASSWORD}'/' ../${PJ_NAME}/.env
