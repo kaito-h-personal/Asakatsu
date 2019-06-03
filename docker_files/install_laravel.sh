d300f23bc1b83d0b8826051e460b74f03deda5f7
kaito-h <kaito.hayafune@pasonadigisol.jp>
2019-06-03 13:11:58 +0900

    sed×3を1つにまとめた

--- docker_files/install_laravel.sh
+++ docker_files/install_laravel.sh
@@ -11,6 +11,6 @@ docker-compose run app composer create-project --prefer-dist laravel/laravel ${P
 
 # Laravelプロジェクトのmysqlの設定
 # 本当はsedのcコマンドを使いたかったが、マニュアル通り動かなかったので妥協...
-sed -ie 's/DB_DATABASE=.*$/DB_DATABASE='${MYSQL_DATABASE}'/' ../${PJ_NAME}/.env
-sed -ie 's/DB_USERNAME=.*$/DB_USERNAME='${MYSQL_USERNAME}'/' ../${PJ_NAME}/.env
-sed -ie 's/DB_PASSWORD=.*$/DB_PASSWORD='${MYSQL_PASSWORD}'/' ../${PJ_NAME}/.env
+sed -ie 's/DB_DATABASE=.*$/DB_DATABASE='${MYSQL_DATABASE}'/; \ 
+         s/DB_USERNAME=.*$/DB_USERNAME='${MYSQL_USERNAME}'/; \
+         s/DB_PASSWORD=.*$/DB_PASSWORD='${MYSQL_PASSWORD}'/' ../${PJ_NAME}/.env
