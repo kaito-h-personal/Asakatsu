1796aaa729952589aad73c3f282746e0e894f7a0
kaito-h <kaito.hayafune@pasonadigisol.jp>
2019-06-14 15:11:25 +0900

    laravelの設定は.envをコピーするように変更(2)

--- docker_files/install_laravel.sh
+++ docker_files/install_laravel.sh
@@ -6,7 +6,4 @@ docker-compose build --no-cache
 # imageからコンテナを作成(起動はしない)(buildもしない)
 docker-compose up --no-build --no-start 
 
-# appコンテナ内からLaravelインストール
-docker-compose run app composer create-project --prefer-dist laravel/laravel ${PJ_NAME}
-
 cp -f .env ../${PJ_NAME}/.env
