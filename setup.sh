9348ff954db3535774818ee88678e5367bb25d34
kaito-h <kaito.hayafune@pasonadigisol.jp>
2019-06-24 18:22:34 +0900

    setup周り修正

--- setup.sh
+++ setup.sh
@@ -10,14 +10,14 @@ docker-compose up -d
 . .env
 
 if [ ! -d "${PJ_NAME}" ]; then
-  echo '# Laravelをインストールします'
+  echo 'installing Laravel...'
   docker-compose run app composer create-project --prefer-dist laravel/laravel ${PJ_NAME}
 fi
 
 # laravelがredisと接続するために必要なライブラリ
-docker-compose run app cd ./${PJ_NAME} && composer require predis/predis
+docker-compose run app echo 'installing predis...' && cd ${PJ_NAME} && composer require predis/predis && cd ../ #appとcdの間に何か挟まないとエラー出る？？？docker-compose runがこちらの思惑通りにコマンドを読めていない？
 
 # Laravelの.envを設定
 cp -f ./docker_files/env/laravel.env ./${PJ_NAME}/.env
 
-echo 'setup finished'
+echo 'setup finished.'
