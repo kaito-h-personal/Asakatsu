785f56d14e9642a5c257b0a3647f8b26e126c9c9
kaito-h <kaito.hayafune@pasonadigisol.jp>
2019-06-24 16:15:18 +0900

    predisのインストールはシェルで行うようにした(できればdocker-compose upでやりたい)

--- docker_files/setup.sh
+++ docker_files/setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 # appのDockerfileからimageを作成
-# cacheを使うとDockerfileのRUNが行われないことがある
+# cacheを使うとDockerfileのRUNが行われないことがある //最後に消す
 docker-compose build --no-cache
 
 # imageからコンテナを作成(buildはしない)
 docker-compose up --no-build 
 
+# Laravelの.envを設定
 #cp -f .env ../${PJ_NAME}/.env
+
+. .env
+//ifexists
+#docker-compose run app composer create-project --prefer-dist laravel/laravel ${PJ_NAME}
+
+docker-compose run app require predis/predis
