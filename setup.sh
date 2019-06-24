9518e6318c7c1d4286130ed6d74d48baad72e185
kaito-h <kaito.hayafune@pasonadigisol.jp>
2019-06-24 16:48:27 +0900

    setup方法を修正

--- setup.sh
+++ setup.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 # appのDockerfileからimageを作成
 # cacheを使うとDockerfileのRUNが行われないことがある //最後に消す
-docker-compose build --no-cache
+#docker-compose build --no-cache
 
-# imageからコンテナを作成(buildはしない)
-docker-compose up --no-build 
+# imageからコンテナを作成
+docker-compose up -d
+
+# read env.
+. .env
+
+if[ ! -d "${}" ]; then
+  echo 'Laravelをインストールします'
+  docker-compose run app composer create-project --prefer-dist laravel/laravel ${PJ_NAME}
+fi
+
+# laravelがredisと接続するために必要なライブラリ
+docker-compose run app cd ./${PJ_NAME} && composer require predis/predis
 
 # Laravelの.envを設定
-#cp -f .env ../${PJ_NAME}/.env
+cp -f ./docker_files/laravel.env ./${PJ_NAME}/.env
 
-. .env
-//ifexists
-#docker-compose run app composer create-project --prefer-dist laravel/laravel ${PJ_NAME}
-
-docker-compose run app require predis/predis
+echo 'setup finished'
