e923f709e49de9790d2a957e432f69de395bf3ae
kaito-h <kaito.hayafune@pasonadigisol.jp>
2019-07-09 10:51:46 +0900

    setup.shのミス修正

--- setup.sh
+++ setup.sh
@@ -11,7 +11,8 @@ if [ ! -d "${PJ_NAME}" ]; then
 fi
 
 # laravelがredisと接続するために必要なライブラリ
-docker-compose run app echo 'installing predis...' && cd src/${PJ_NAME} && composer require predis/predis && cd ../ #appとcdの間に何か挟まないとエラー出る？？？docker-compose runがこちらの思惑通りにコマンドを読めていない？
+# プロジェクトディレクトリ内で行う必要があり、マウント後に実行する必要があったため止むを得ずdocker-compose runで実行
+docker-compose run app echo 'installing predis...' && ${PJ_NAME} && composer require predis/predis && cd ../ #appとcdの間に何か挟まないとエラー出る？？？docker-compose runがこちらの思惑通りにコマンドを読めていない？
 
 # Laravelの.envを設定
 cp -f ./docker_files/env/laravel.env ./src/${PJ_NAME}/.env
