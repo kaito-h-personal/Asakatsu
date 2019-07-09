d795cd31fb66c0b2ef8869fbbff1d651f43cecfe
kaito-h <kaito.hayafune@pasonadigisol.jp>
2019-07-09 11:18:22 +0900

    setup.sh更新

--- setup.sh
+++ setup.sh
@@ -5,14 +5,16 @@ docker-compose up -d
 # read env.
 . .env
 
-if [ ! -d "${PJ_NAME}" ]; then
+# './setup.sh --install-pj'でlaravelプロジェクトをインストール
+if [ "$1" = "--install-pj" ]; then
   echo 'installing Laravel...'
   docker-compose run app composer create-project --prefer-dist laravel/laravel ${PJ_NAME}
 fi
 
 # laravelがredisと接続するために必要なライブラリ
 # プロジェクトディレクトリ内で行う必要があり、マウント後に実行する必要があったため止むを得ずdocker-compose runで実行
-docker-compose run app echo 'installing predis...' && ${PJ_NAME} && composer require predis/predis && cd ../ #appとcdの間に何か挟まないとエラー出る？？？docker-compose runがこちらの思惑通りにコマンドを読めていない？
+echo 'installing predis...'
+docker-compose run --workdir="/var/www/html/${PJ_NAME}" app composer require predis/predis #appとcdの間に何か挟まないとエラー出る？？？docker-compose runがこちらの思惑通りにコマンドを読めていない？
 
 # Laravelの.envを設定
 cp -f ./docker_files/env/laravel.env ./src/${PJ_NAME}/.env
