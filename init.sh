1622aba72aab1588c81184a1c4d1ef387c7581d4
kaito-h <kaito.hayafune@pasonadigisol.jp>
2019-09-19 10:39:42 +0900

    コンテナ名を.envで指定

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ grep -e PJ_NAME \
      -e DB_USERNAME \
      -e DB_PASSWORD \
      docker_files/env/env_all > .env
+grep PJ_NAME .env | sed -e "s/PJ_NAME/COMPOSE_PROJECT_NAME/" >> .env
 
 . .env
 docker-compose up -d
